4f4eaa0582078237ccf8b3af1bfdca7538af118b
René Giller <rene.giller@bit.admin.ch>
2023-05-05 18:30:53 +0200

    fix(service-navigation): remove unwanted margin on authentication
    Due to the new design, no margin should be added

--- projects/oblique/src/lib/service-navigation/authentication/service-navigation-authentication.component.ts
+++ projects/oblique/src/lib/service-navigation/authentication/service-navigation-authentication.component.ts
@@ -3,7 +3,6 @@ import {Component, Input, ViewEncapsulation} from '@angular/core';
 @Component({
 	selector: 'ob-service-navigation-authentication',
 	templateUrl: './service-navigation-authentication.component.html',
-	styleUrls: ['./service-navigation-authentication.component.scss'],
 	encapsulation: ViewEncapsulation.None,
 	host: {class: 'ob-service-navigation-authentication'}
 })
