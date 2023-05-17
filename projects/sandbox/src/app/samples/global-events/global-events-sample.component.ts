f60706b2bfa8f246d8f79544077f6a627a1b7315
René Giller <rene.giller@bit.admin.ch>
2023-05-17 06:42:03 +0200

    sandbox: rename `showcase` into `sandbox` and move it into the `projects` directory
    OUI-2326

---
+++ projects/sandbox/src/app/samples/global-events/global-events-sample.component.ts
@@ -0,0 +1,17 @@
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {ObGlobalEventsService, obOutsideFilter} from '@oblique/oblique';
+
+@Component({
+	selector: 'sb-utilities-sample',
+	templateUrl: './global-events-sample.component.html'
+})
+export class GlobalEventsSampleComponent implements AfterViewInit {
+	@ViewChild('outsideClick', {read: ElementRef}) button: ElementRef;
+	events = [];
+
+	constructor(public readonly globalEvents: ObGlobalEventsService) {}
+
+	ngAfterViewInit(): void {
+		this.globalEvents.click$.pipe(obOutsideFilter(this.button.nativeElement)).subscribe(event => this.events.push(event.target));
+	}
+}
