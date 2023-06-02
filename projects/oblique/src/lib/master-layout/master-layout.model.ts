e9fc7ed49098a79a514852276cf84b7d6fcec460
Albert Marsnik <albert.marsnik@bit.admin.ch>
2023-06-02 17:22:51 +0200

    feat(oblique/master-layout): add option to move links to right side
    - `startOfRightSideLinks` property added to `ObINavigationLink` & `ObNavigationLink`
    - when true this sets `ob-start-of-right-side-links` class on link
    - `ob-start-of-right-side-links` shifts itself and all following links to the right
    
    OUI-2318

--- projects/oblique/src/lib/master-layout/master-layout.model.ts
+++ projects/oblique/src/lib/master-layout/master-layout.model.ts
@@ -78,6 +78,7 @@ export interface ObINavigationLink {
 	routerLinkActiveOptions?: IsActiveMatchOptions;
 	icon?: ObEIcon | string;
 	iconOnly?: boolean;
+	startOfRightSideLinks?: boolean;
 }
 
 export interface ObISkipLink {
